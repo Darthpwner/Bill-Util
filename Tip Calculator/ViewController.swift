add3d8efd9939ae314de834a1d9a727d176c9438
Darthpwner <matthewallenlin@gmail.com>
2017-01-21 15:07:09 -0800

    Try to add PickerHandler

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     // Tip percentages
     var pickerDataSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "27", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
     
+    var tipPickerView: UIPickerView = UIPickerView()
+    
     @IBOutlet weak var tipAmountField: UITextField!
     @IBOutlet weak var totalField: UITextField!
     @IBOutlet weak var tipAmountPerPersonField: UITextField!
@@ -35,6 +37,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
 
         view.addGestureRecognizer(tap)
+        
+        
     }
 
     //Calls this function when the tap is recognized.
@@ -48,7 +52,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
     
-    //PickerView Methods
+    //Custom PickerView methods
+    func pickerViewHandler () {
+        self.tipPickerView.isHidden = true
+        self.tipPickerView.dataSource = self
+        self.tipPickerView.delegate = self
+        self.tipPickerView.frame = CGRect(x: 100, y: 100, width: 100, height: 162)
+        self.tipPickerView.backgroundColor = UIColor.black
+        self.tipPickerView.layer.borderColor = UIColor.white.cgColor
+        self.tipPickerView.layer.borderWidth = 1
+    }
+    //End of custom PickerView methods
+    
+    //Boilerplate PickerView Methods
     func numberOfComponents(in: UIPickerView) -> Int {
         return 5
     }
