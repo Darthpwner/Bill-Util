ee9df8a8c25fb8e8cb29ed1d5f9f387d1fa9c014
Darthpwner <matthewallenlin@gmail.com>
2017-01-22 02:11:37 -0800

    Done with currency converter

--- Tip Calculator/CurrencyConverterViewController.swift
+++ Tip Calculator/CurrencyConverterViewController.swift
@@ -81,5 +81,83 @@ class CurrencyConverterViewController: UIViewController, UIPickerViewDelegate, U
         print(pickerValue)
     }
 
-
+    @IBAction func convertCurrency(_ sender: Any) {
+        guard let queryAmount = Double(queryTextField.text!) else {
+            //show error
+            usDollarTextField.text = ""
+            euroTextField.text = ""
+            yenTextField.text = ""
+            poundTextField.text = ""
+            francTextField.text = ""
+            canadianDollarTextField.text = ""
+            return
+        }
+        
+        var usDollarAmount = Double(usDollarTextField.text!)
+        var euroAmount = Double(euroTextField.text!)
+        var yenAmount = Double(yenTextField.text!)
+        var poundAmount = Double(poundTextField.text!)
+        var francAmount = Double(francTextField.text!)
+        var canadianDollarAmount = Double(canadianDollarTextField.text!)
+        
+        switch pickerValue {
+            case "US $":
+                usDollarAmount = queryAmount
+                euroAmount = queryAmount * 0.93
+                yenAmount = queryAmount * 114.62
+                poundAmount = queryAmount * 0.81
+                francAmount = queryAmount
+                canadianDollarAmount = queryAmount * 1.33
+            case "Euro":
+                usDollarAmount = queryAmount * 1.07
+                euroAmount = queryAmount
+                yenAmount = queryAmount * 122.67
+                poundAmount = queryAmount * 0.86
+                francAmount = queryAmount * 1.07
+                canadianDollarAmount = queryAmount * 1.43
+            case "Yen":
+                usDollarAmount = queryAmount * 0.0087
+                euroAmount = queryAmount * 0.0082
+                yenAmount = queryAmount
+                poundAmount = queryAmount * 0.007
+                francAmount = queryAmount * 0.0087
+                canadianDollarAmount = queryAmount * 0.012
+            case "Pound":
+                usDollarAmount = queryAmount * 1.24
+                euroAmount = queryAmount * 1.16
+                yenAmount = queryAmount * 141.91
+                poundAmount = queryAmount * 1.24
+                francAmount = queryAmount
+                canadianDollarAmount = queryAmount * 1.65
+            case "Franc":
+                usDollarAmount = queryAmount
+                euroAmount = queryAmount * 0.93
+                yenAmount = queryAmount * 114.42
+                poundAmount = queryAmount * 0.81
+                francAmount = queryAmount
+                canadianDollarAmount = queryAmount * 1.33
+            case "CA $":
+                usDollarAmount = queryAmount * 0.75
+                euroAmount = queryAmount * 0.7
+                yenAmount = queryAmount * 86.06
+                poundAmount = queryAmount * 0.61
+                francAmount = queryAmount * 0.75
+                canadianDollarAmount = queryAmount
+            default:
+                usDollarAmount = 0
+                euroAmount = 0
+                yenAmount = 0
+                poundAmount = 0
+                francAmount = 0
+                canadianDollarAmount = 0
+        }
+        
+        usDollarTextField.text = String(format: "%.2f", usDollarAmount!)
+        euroTextField.text = String(format: "%.2f", euroAmount!)
+        yenTextField.text = String(format: "%.2f", yenAmount!)
+        poundTextField.text = String(format: "%.2f", poundAmount!)
+        francTextField.text = String(format: "%.2f", francAmount!)
+        canadianDollarTextField.text = String(format: "%.2f", canadianDollarAmount!)
+    }
+    
 }
