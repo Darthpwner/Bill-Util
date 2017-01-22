96f4d4f9e28162ac12dd35dd66ed6839c21f4109
Darthpwner <matthewallenlin@gmail.com>
2017-01-21 16:53:44 -0800

    Added ability to control the picker

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var setTipPercentageButton: UIButton!
     
     // Tip percentages
-    var pickerDataSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "27", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
+    var pickerDataSource = ["0% (No Tip)", "1%", "2%", "3%", "4%", "5% (Bad)", "6%", "7%", "8%", "9%", "10% (Average)", "11%", "12%", "13%", "14%", "15% (Good)", "16%", "17%", "18%", "19%", "20% (Great)", "21%", "22%", "23%", "24%", "25% (Exceptional)", "26%", "27%", "28%", "29%", "30%", "31%", "32%", "33%", "34%", "35%", "36%", "37%", "38%", "39%", "40%", "41%", "42%", "43%", "44%", "45%", "46%", "47%", "48%", "49%", "50% (Top Notch)", "51%", "52%", "53%", "54%", "55%", "56%", "57%", "58%", "59%", "60%", "61%", "62%", "63%", "64%", "65%", "66%", "67%", "68%", "69%", "70%", "71%", "72%", "73%", "74%", "75%", "76%", "77%", "78%", "79%", "80%", "81%", "82%", "83%", "84%", "85%", "86%", "87%", "88%", "89%", "90%", "91%", "92%", "93%", "94%", "95%", "96%", "97%", "98%", "99%", "100% (Peerless)"]
     
     var tipPickerView: UIPickerView = UIPickerView()
     
@@ -60,11 +60,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         self.tipPickerView.dataSource = self
         self.tipPickerView.delegate = self
         
-//        let screenSize = UIScreen.main.bounds
-//        let screenWidth = screenSize.width
-//        let screenHeight = screenSize.height
-        
-        self.tipPickerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 10)
+
+        self.tipPickerView.frame = CGRect(x: 0, y: 520, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 5)
         self.tipPickerView.backgroundColor = UIColor.white
         self.tipPickerView.layer.borderColor = UIColor.white.cgColor
         self.tipPickerView.layer.borderWidth = 1
@@ -293,13 +290,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }else if(row == 100) {
             tipPercentageProgressView.progress = 1.00
         }
+        
+        tipPercentageLabel.text = "\(Int(tipPercentageProgressView.progress * 100))"
     }
     //
     
     @IBAction func setTipPercentage(_ sender: Any) {
         //Show a Picker View here
+        if(tipPickerView.isHidden) {
+            tipPickerView.isHidden = false
+        } else {
+            tipPickerView.isHidden = true
+        }
         
-        tipPickerView.isHidden = false
         print("TEST")
     }
 
