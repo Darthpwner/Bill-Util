1bee50b4bc3a7a7d8e9a137e95723b0808de5fff
Darthpwner <matthewallenlin@gmail.com>
2017-01-21 21:26:05 -0800

    Added ability to remove keyboard on Income Calculator

--- Tip Calculator/IncomeViewController.swift
+++ Tip Calculator/IncomeViewController.swift
@@ -14,7 +14,19 @@ class IncomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
     }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
